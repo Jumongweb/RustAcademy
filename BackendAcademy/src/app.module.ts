4afe1c37e5a841fc58934fd32acb484739b063ef
akordavid373 <sandrawillow001-afk@users.noreply.github.com>
2026-06-26 13:26:48 +0100

    feat: add tutor profile entity with reputation and specialty attributes

--- BackendAcademy/src/app.module.ts
+++ BackendAcademy/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TutorProfileModule } from './users/tutor-profile.module';
 
 @Module({
-  imports: [],
+  imports: [TutorProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
