9f31efd762762b41e5d4934b41fcd8c4947fbc11
Your Name <you@example.com>
2026-06-27 12:28:05 +0100

    feat: add chat module skeleton

--- BackendAcademy/src/app.module.ts
+++ BackendAcademy/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TutorProfileModule } from './users/tutor-profile.module';
 import { SubmissionModule } from './submissions/submission.module';
 import { RewardsModule } from './rewards/rewards.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [TutorProfileModule, SubmissionModule, RewardsModule],
+  imports: [TutorProfileModule, SubmissionModule, RewardsModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
