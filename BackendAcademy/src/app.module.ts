3e087c2401e5b3c8d7efcbdbfe0a00bc066f30b2
Sadeequ <70214653+Sadeequ@users.noreply.github.com>
2026-06-29 13:18:27 +0000

    Add user preferences and admin analytics endpoints

--- BackendAcademy/src/app.module.ts
+++ BackendAcademy/src/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TutorProfileModule } from './users/tutor-profile.module';
+import { UsersModule } from './users/users.module';
 import { SubmissionModule } from './submissions/submission.module';
 import { RewardsModule } from './rewards/rewards.module';
 import { ChallengesModule } from './challenges/challenges.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [TutorProfileModule, SubmissionModule, RewardsModule, ChallengesModule],
+  imports: [
+    TutorProfileModule,
+    UsersModule,
+    SubmissionModule,
+    RewardsModule,
+    ChallengesModule,
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
